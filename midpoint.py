92c439facfdf983d736779013516fc3f9a9308cd
GP <123821876+GeethanPrasath147@users.noreply.github.com>
2026-09-24 21:04:00 +0530

    Implement midpoint calculation from user inputs
    Calculate the midpoint of two endpoints based on user input.

---
+++ midpoint.py
@@ -0,0 +1,10 @@
+#midpoint of 2 endpoints
+x1=int(input())
+x2=int(input())
+y1=int(input())
+y2=int(input())
+
+midx=(x1+y1)/2
+midy=(x2+y2)/2
+
+print(f"Arun's house is located at({midx:.1f},{midy:.1f})")
