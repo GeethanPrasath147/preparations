d56c3abbfa9bc24fc6b766974109269355cd55e6
GP <123821876+GeethanPrasath147@users.noreply.github.com>
2026-09-24 15:51:31 +0530

    Add area and perimeter calculation script

---
+++ area&perimeter.py
@@ -0,0 +1,6 @@
+#Area and perimeter LC
+length=int(input())
+breadth=int(input())
+
+print("The required length is",2* (length + breadth),"m")
+print("The required area of carpet is",length * breadth,"sqm")
