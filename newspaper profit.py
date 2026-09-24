96d7cbb5c4a58122bb4fb806096a005c95a5578c
GP <123821876+GeethanPrasath147@users.noreply.github.com>
2026-09-24 15:59:51 +0530

    Implement profit calculation for newspaper sales
    Calculate profit from newspaper sales based on copies sold, cost per copy, and expenses.

---
+++ newspaper profit.py
@@ -0,0 +1,6 @@
+#Input consists of 3 integers: a, b, and c. a is the number of copies sold, b is the cost per copy and c is the cost The Herald spends per copy.
+a=int(input())
+b=int(input())
+c=int(input())
+
+print((a*b)-((a*c))-100)
